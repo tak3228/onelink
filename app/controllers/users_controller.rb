2fa11992788e6b05b2e124701fc4d2d31656efca
Takayuki Hirayama <hirayama@fiilse.com>
2016-07-25 19:15:10 +0900

    user page added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+	before_action :set_user, only: [:show, :edit, :update]
+	before_action :current_user, only: [:edit, :update]
   def show
 	  @user = User.find(params[:id])
   end
@@ -9,11 +12,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-	    flash[:success] = "Welcome to the Sample App!"
+	    flash[:success] = "さぁ、始めましょう!"
 	    redirect_to @user
     else
       render 'new'
     end
+  end
+  def edit
+	  @user = User.find(params[:id])
+  end
+  def update
+	  @user = User.find(params[:id])
+	  if @user.update(user_params)
+		  flash[:success] = "変更内容を更新しました"
+		  redirect_to @user
+	  else
+		  render 'edit'
+	  end
   end
 
   private
@@ -21,5 +36,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  def set_user
+	  @user = User.find(params[:id])
   end
 end
