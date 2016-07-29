23c010aabdd919b267950a88240d369d116429b9
Takayuki Hirayama <hirayama@fiilse.com>
2016-07-29 23:40:32 +0900

    0729

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
-  def home
-	  
-		  @link = current_user.links.build if logged_in?
-		  @links = current_user.links.all
-
-  end
+	def home
+		if logged_in? 
+			@link = current_user.links.build if logged_in?
+			@links = current_user.links.all
+		end
+	end
 end
