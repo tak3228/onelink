f2e64a2ddca122f7e41bc13c9df9020b1763197a
Takayuki Hirayama <hirayama@fiilse.com>
2016-08-03 16:03:15 +0900

    1600

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
-	  @link = current_user.links.build if logged_in?
-	  
-	  
-	 # @links = Link.all
-	 @links = []
-	 bunruis = Origin.all.pluck(:id)
-	 bunruis.each do |bunrui|
-	     link_temp = Link.where(bunrui: bunrui).flatten
-	     @links << link_temp
-     end
+    @link = current_user.links.build if logged_in?
+
+
+    # @links = Link.all
+    @links = []
+    bunruis = Origin.all.pluck(:id)
+    bunruis.each do |bunrui|
+      link_temp = current_user.links.where(bunrui: bunrui).flatten if logged_in?
+      @links << link_temp
+    end
   end
 end
