cb687135c6506e315e8a8c7a4ffa1e5f6a2bb1d7
Takayuki Hirayama <hirayama@fiilse.com>
2016-08-04 19:32:48 +0900

    1900

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -8,22 +8,19 @@ class LinksController < ApplicationController
   def create
     @link = Link.new(link_params)
     @link.user_id = current_user.id
-    # @link.url = params[:link][:url]
     @link.title = @link.getpagetitle
-    # @link.title = params[:link][:title]
-    # @link.bunrui = params[:link][:id]
     if @link.save
       flash[:success] = 'リンクが作成されました'
       redirect_to root_path
     else
       render 'new'
-      # redirect_to new_link_path ←リダイレクトさせてしまうと@link.errorsの内容が消えてしまう
     end
   end
   
   def destroy
     @link = Link.find(params[:id])
     @link.destroy
+		flash[:success] = 'リンクを削除しました'
     redirect_to :root
   end
 
