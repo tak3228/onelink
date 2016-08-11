75e3201ba38d4559592a78b20e9dc889a6810688
Takayuki Hirayama <hirayama@fiilse.com>
2016-08-11 16:15:25 +0900

    0811

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,33 +1,52 @@
 class LinksController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create]
+	before_action :logged_in_user, only: [:new, :create]
+	before_action :set_link, only: [:edit, :update]
 
-  def new
-    @link = Link.new
-  end
+	def new
+		@link = Link.new
+	end
 
-  def create
-    @link = Link.new(link_params)
-    @link.user_id = current_user.id
-    @link.title = @link.getpagetitle
-    if @link.save
-      flash[:success] = 'リンクが作成されました'
-      redirect_to root_path
-    else
-      render 'new'
-    end
-  end
-  
-  def destroy
-    @link = Link.find(params[:id])
-    @link.destroy
-		flash[:success] = 'リンクを削除しました'
-    redirect_to :root
-  end
+	def edit
+	end
+	def update
+		if @link.update(link_params)
+			# 保存に成功した場合はトップページへリダイレクト
+			redirect_to root_path , notice: 'メッセージを編集しました'
+		else
+			# 保存に失敗した場合は編集画面へ戻す
+			render 'edit'
+		end
+	end
+
+		def create
+			@link = Link.new(link_params)
+			@link.user_id = current_user.id
+			if @link.valid?(:url)#valid?はrailsで有効であるかどうか検証する、単独使用可。
+				@link.title = @link.get_page_title
+			end
+			if @link.save
+				flash[:success] = 'リンクが作成されました'
+				redirect_to root_path
+			else
+				render 'new'
+			end
+		end
+
+		def destroy
+			@link = Link.find(params[:id])
+			@link.destroy
+			flash[:success] = 'リンクを削除しました'
+			redirect_to :root
+		end
 
 
-  private
+		private
 
-  def link_params
-    params.require(:link).permit(:url, :title, :bunrui)
-  end
+		def link_params
+			params.require(:link).permit(:url, :title, :bunrui)
+		end
+
+		def set_link
+			@link = Link.find(params[:id])
+		end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -6,33 +6,17 @@ class Link < ActiveRecord::Base
   validates :url, presence: true, format: /\A#{URI.regexp(%w(http https))}\z/
   validates :bunrui, presence: true
   has_many :origin
-  validate :getpagetitle
 
-  def url_exist?(url)
-    res = lambda do |url|
-      begin
-        uri = URI.parse(url)
-        uri_path = uri.path.empty? ? '/' : uri.path
-        Net::HTTP.start(uri.host, uri.port) { |http| http.get(uri_path) }
-      rescue TypeError, SocketError, URI::InvalidURIError
-        false
-      end
+  def get_page_title
+    charset = 'utf-8'
+    html = open(url) do |f|
+      charset = f.charset # 文字種別を取得
+      f.read # htmlを読み込んで変数htmlに渡す
     end
-    !url.nil? && !url.empty? && (Net::HTTPSuccess === res.call(url) || Net::HTTPRedirection === res.call(url))
-  end
+    # htmlをパース(解析)してオブジェクトを生成
+    doc = Nokogiri::HTML.parse(html, nil, charset)
 
-  def getpagetitle
-    if url_exist?(:url)
-      charset = 'utf-8'
-      html = open(url) do |f|
-        charset = f.charset # 文字種別を取得
-        f.read # htmlを読み込んで変数htmlに渡す
-      end
-      # htmlをパース(解析)してオブジェクトを生成
-      doc = Nokogiri::HTML.parse(html, nil, charset)
-
-      # タイトルを取り出す
-      doc.title
+    # タイトルを取り出す
+    doc.title
     end
   end
-end
