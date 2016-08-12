87d8f4ecc962f7215a10aada01d93d3657fa864c
Takayuki Hirayama <hirayama@fiilse.com>
2016-08-12 16:54:45 +0900

    test1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,2 @@
-origin1 = Origin.new(link_id: "1", content: "お気に入り")
-origin2 = Origin.new(link_id: "2", content: "あとで見る")
-origin1.save
-origin2.save
+Origin.create(link_id: "1", content: "お気に入り")
+Origin.create(link_id: "2", content: "あとで見る")
