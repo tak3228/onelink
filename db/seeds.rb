5d50523ceb1d4426ed614a71b2e1618de3963e09
Takayuki Hirayama <hirayama@fiilse.com>
2016-08-12 16:46:40 +0900

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
-Origin.new(link_id: "1", content: "お気に入り")
-Origin.new(link_id: "2", content: "あとで見る")
-
+origin1 = Origin.new(link_id: "1", content: "お気に入り")
+origin2 = Origin.new(link_id: "2", content: "あとで見る")
+origin1.save
+origin2.save
