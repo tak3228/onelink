0a5bdd43476d3615a05072f102689a93b177752b
Takayuki Hirayama <hirayama@fiilse.com>
2016-09-05 22:17:31 +0900

    0905:2200

--- app/controllers/origins_controller.rb
+++ app/controllers/origins_controller.rb
@@ -1,7 +1,25 @@
 class OriginsController < ApplicationController
 	before_action :logged_in_user
+	before_action :set_origin, only: [:edit, :update]
 	def new 
 		@origin = Origin.new
+	end
+
+	def show
+		
+	end
+
+	def update
+		if @origin.update(origin_params)
+			flash[:success] = '変更を保存しました'
+			redirect_to origin_path
+		else
+			render 'edit'
+		end
+	end
+
+	def edit
+		@links = Link.where(bunrui: @origin)
 	end
 
 	def create
@@ -9,7 +27,7 @@ class OriginsController < ApplicationController
 		@origin.user_id = current_user.id
 	  if @origin.save
 			flash[:success] = 'タグを作成しました'
-			redirect_to root_path
+			redirect_to origin_path(current_user) 
 		else
 			render 'new'
 		end
@@ -17,9 +35,9 @@ class OriginsController < ApplicationController
 
 	def destroy
 		@tags = Origin.find(params[:id])
-		@tags.destroy
-		flash[:success] = 'タグを削除しました'
-		redirect_to root_path
+			@tags.destroy
+			flash[:success] = 'タグを削除しました'
+			redirect_to origin_path 
 	end
 
 
@@ -30,5 +48,8 @@ class OriginsController < ApplicationController
 
 	def origin_params
 		params.require(:origin).permit(:content)
+	end
+	def set_origin
+		@origin = Origin.find(params[:id])
 	end
 end
--- app/models/origin.rb
+++ app/models/origin.rb
@@ -1,5 +1,5 @@
 class Origin < ActiveRecord::Base
   belongs_to :link
 	belongs_to :user
-  validates  :content, presence: true, length: { maximum: 140 }
+  validates  :content, presence: true, length: { maximum: 50 }
 end
