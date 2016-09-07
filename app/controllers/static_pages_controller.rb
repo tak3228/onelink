f786fcf0a8eb12425f5f1d0c5713954f4032ba76
Takayuki Hirayama <hirayama@fiilse.com>
2016-09-07 14:02:18 +0900

    bug fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,13 @@ class StaticPagesController < ApplicationController
       link_temp = current_user.links.where(bunrui: bunrui).flatten if logged_in?
       @links << link_temp.reverse if link_temp
     end
+
+		@link_box= []
+		@links.each do |link|
+			link.each do |k|
+			@link_box << k
+		end
+		end
+
   end
 end
